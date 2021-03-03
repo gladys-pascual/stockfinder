6fe239f145eba8a4c31c3f281a0f381e1f4b881f
gladys-pascual <gladyskate@gmail.com>
2021-03-03 00:14:38 +0000

    Finalise auto complete in search component. Implementetd debounce function where API is only called after 0.5 sec when the user stopped typing, instead of calling the API on every letter the user types.

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,22 +5,20 @@ import axios from "axios";
 import LoadingSearchAPI from "../Loading/LoadingSearchAPI";
 
 const Search = () => {
-  const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const [selectedCompany, setSelectedCompany] = useState(null);
   const [loading, setLoading] = useState(false);
-
   let history = useHistory();
 
-  async function getCompanyData() {
+  async function getCompanyData(inputValue) {
     setLoading(true);
     try {
       const results = await axios.get(
-        `https://finnhub.io/api/v1/search?q=${query}&token=${process.env.REACT_APP_API_KEY}`
+        `https://finnhub.io/api/v1/search?q=${inputValue}&token=${process.env.REACT_APP_API_KEY}`
       );
-      console.log(results);
       const firstFiveResults = results.data.result.slice(0, 5);
       setSearchResults(firstFiveResults);
+      setSelectedCompany(firstFiveResults[0]);
       setLoading(false);
     } catch (err) {
       console.log("Error fetching and parsing data", err);
@@ -28,41 +26,72 @@ const Search = () => {
     }
   }
 
-  const handleInputChange = (e) => {
-    setQuery(e.target.value);
-    if (query && query.length > 0) {
-      getCompanyData();
-    }
+  // Returns a function, that, as long as it continues to be invoked, will not be triggered.
+  // The function will be called after it stops being called for `wait` milliseconds.
+  const debounce = (func, wait) => {
+    let timeout;
+    // This is the function that is returned and will be executed many times
+    return function executedFunction(...args) {
+      const later = () => {
+        timeout = null;
+        func(...args);
+      };
+      // This will reset the waiting every function execution.
+      clearTimeout(timeout);
+      // Restart the debounce waiting period. setTimeout returns a truthy value (it differs in web vs Node)
+      timeout = setTimeout(later, wait);
+    };
   };
 
+  const debouncedInputChange = debounce((e) => {
+    const inputValue = e.target.value;
+    if (inputValue && inputValue.length > 0) {
+      getCompanyData(inputValue);
+    } else {
+      setSearchResults([]);
+      setSelectedCompany(null);
+    }
+  }, 500);
+
   const hanldeItemClick = (item) => {
-    console.log(item);
     setSelectedCompany(item);
-    setQuery("");
     history.push(`/quote/${item.symbol}`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (selectedCompany) {
+      history.push(`/quote/${selectedCompany.symbol}`);
+    }
   };
 
   const results = searchResults.map((item, i) => {
     return (
-      <li className="result-item" key={i} onClick={() => hanldeItemClick(item)}>
+      <li
+        className={
+          (selectedCompany && selectedCompany.symbol) === item.symbol
+            ? "active result-item"
+            : "result-item"
+        }
+        key={i}
+        onClick={() => hanldeItemClick(item)}
+      >
         <p className="company-name">{item.description} </p>
         <p className="company-symbol">{item.symbol}</p>
       </li>
     );
   });
 
-  console.log(selectedCompany);
-
   return (
     <>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-wrapper">
           <input
             className="search-input"
             type="text"
             name="query"
             placeholder="Search for symbol or company name..."
-            onChange={handleInputChange}
+            onChange={debouncedInputChange}
           ></input>
           <button type="submit" id="submit">
             <p>Search</p>
